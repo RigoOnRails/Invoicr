a6b5d115c9d953985faa39a1768f1241500e10c2
Rigoberto Molina <rigothedev@gmail.com>
2022-08-31 06:16:08 -0700

    Add name attribute to users

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable, :lockable
+
+  validates :name, presence: true
 end
